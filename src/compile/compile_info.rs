0802e7c820e2d0aa8f6c862a5d696393294318ea
zhangjianshe <zhangjianshe@gmail.com>
2025-01-19 10:33:53 +0800

    0.1.5 load info automatic.

--- src/compile/compile_info.rs
+++ src/compile/compile_info.rs
@@ -1,14 +1,20 @@
+use std::process::Command;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize, Default)]
 pub struct CompileInfo {
-    pub git_hash: String,
-    pub rust_version: String,
+    pub git_commit_email: String,
+    pub git_commit_hash: String,
+    pub git_commit_message:String,
     pub git_commit_time: String,
+    pub rust_version: String,
     pub build_time: String,
+    pub build_os:String
 }
 
 impl CompileInfo {
+    /// load from string return a new CompileInfo object.
+    ///
     pub fn load_from_str(compile_info: &str) -> Result<CompileInfo, String> {
         let info = serde_json::from_str(&compile_info);
         match info {
@@ -16,7 +22,45 @@ impl CompileInfo {
             Err(err) => Err(format!("{:?}", err)),
         }
     }
-    pub fn save_to_str(&self) -> String {
-        serde_json::to_string(&self).unwrap()
+    /// save this object to a string
+    ///
+    pub fn save_to_str(&self,pretty:bool) -> String {
+        if  pretty{
+            serde_json::to_string(&self).unwrap()
+        }
+        else {
+            format!("{}", serde_json::to_string_pretty(&self).unwrap())
+        }
+    }
+
+    /// execute some command from the environments
+    pub fn load_from_env()->Self{
+        let output = Command::new("git")
+            .args(&["git", "log", "HEAD","--format=%h%n%s%n%ce%n%ci"])
+            .output()
+            .expect("Failed to execute git log command");
+
+        let git_log = String::from_utf8(output.stdout)
+            .expect("Failed to convert git log output to string");
+        let lines=git_log.lines().collect::<Vec<&str>>();
+
+        let build_os=if cfg!(target_os = "windows") {
+            "windows"
+        } else if cfg!(target_os = "linux") {
+            "linux"
+        } else { "unknown" };
+
+        let rustc_cmd=Command::new("rustc").args(&["--version"]).output().expect("Failed to execute rustc");
+        let rustc_output=String::from_utf8(rustc_cmd.stdout).expect("Failed to convert rustc output to string");
+
+        CompileInfo{
+            git_commit_email:lines[2].to_string(),
+            git_commit_hash: lines[0].to_string(),
+            git_commit_message: lines[1].to_string(),
+            git_commit_time: lines[3].to_string(),
+            rust_version:rustc_output,
+            build_time: chrono::Utc::now().to_rfc3339(),
+            build_os:build_os.to_string()
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,27 @@
 //!
 //! This crate collects building information from all kinds of environments in to an object [CompileInfo]
 //!
+//! generally, this is used in the build.rs file
+//! when build project,we write the information to a file like 'src/context/compile_info.txt'
+//! then we can use it in our program with
+//! CompileInfo::load_from_str(!include("/src/context/compile_info.txt"));
+//!
+//! CompileInfo serialize and Deserialize by serde_json library.
 //!
 //!
-
+//! #[example]
+//!
+//! ```
+//! use serde::{Deserialize, Serialize};
+//! use std::process::Command;use satway_build::CompileInfo;
+//!
+//! let compile_info=CompileInfo::load_from_env();
+//! let json_str=compile_info.save_to_str(true);
+//! println!("{}",json_str);
+//!
+//! ```
+//!
+//!
 mod compile;
-
 pub use compile::CompileInfo;
 
