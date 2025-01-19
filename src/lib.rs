9a294e659375fb23dda657402a4539604e3efb08
zhangjianshe <zhangjianshe@gmail.com>
2025-01-20 03:37:37 +0800

    add some example

--- src/lib.rs
+++ src/lib.rs
@@ -13,13 +13,34 @@
 //!
 //! #[example]
 //!
+//! build.rs  write build information to a file
 //! ```
-//! use serde::{Deserialize, Serialize};
-//! use std::process::Command;use satway_build::CompileInfo;
 //!
-//! let compile_info=CompileInfo::load_from_env();
-//! let json_str=compile_info.save_to_str(true);
-//! println!("{}",json_str);
+//! use satway_build::CompileInfo;
+//! use std::fmt::Debug;
+//! use std::path::Path;
+//! use std::{env, fs};
+//!
+//! pub fn main() {
+//!
+//!     println!("cargo:rerun-if-changed=build.rs");
+//!     let compile_info: CompileInfo = CompileInfo::load_from_env();
+//!
+//!     let out_file_path = Path::new(&env::var("OUT_DIR").unwrap()).join("compile_info.txt");
+//!     println!("cargo:rustc-env=COMPILE_INFO_FILE={}", out_file_path.as_path().to_str().unwrap());
+//!     fs::write(out_file_path, compile_info.save_to_str(true)).expect("Unable to write file");
+//! }
+//! ```
+//!
+//! use it in an api server
+//! ```
+//! use satway_build::CompileInfo;
+//!
+//! pub fn version_info() {
+//!     let json = include_str!(env!("COMPILE_INFO_FILE"));
+//!     let compile_info = CompileInfo::load_from_str(json);
+//!     println!("{:#?}", compile_info);
+//! }
 //!
 //! ```
 //!
